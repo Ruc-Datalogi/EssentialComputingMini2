8ac2895f9c5db94ef724c9c903e6271d703a2115
MikkelEsb <55182735+mikkelesb@users.noreply.github.com>
2020-11-20 10:44:05 +0100

    Added spaces to synom checker so we don't change parts of words which has "im" in it.

--- src/SynonymCheck.java
+++ src/SynonymCheck.java
@@ -1,9 +1,9 @@
 public class SynonymCheck {
 
     String checkForSynonym(String line){
-        line = line.replaceAll("i am", "i'm");
-        line = line.replaceAll("im","i'm");
-        line = line.replaceAll("you are ", "you're");
+        line = line.replaceAll(" i am ", " i'm ");
+        line = line.replaceAll(" im "," i'm ");
+        line = line.replaceAll(" you are ", " you're ");
         System.out.println("i change " + line);
         return line;
     }
